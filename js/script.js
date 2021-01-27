2a8985cc153eb8ee7917ca77fbe7c8757f937b6c
Egor <68065928+E-g-oR@users.noreply.github.com>
2021-01-27 19:20:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,79 +1,35 @@
-// const requestURL = 'https://www.nbrb.by/api/exrates/currencies';
+// const requestURL = 'https://www.nbrb.by/api/exrates/currencies';  //! Полный список валют на оф сайте нцбанка РБ
+// !  Адреса запросов по валютам
+// const requestURLs = [
+//   'https://www.nbrb.by/api/exrates/rates/292',      // Евро
+//   'https://www.nbrb.by/api/exrates/rates/145',      // Доллар США
+//   'https://www.nbrb.by/api/exrates/rates/298',      // Российский рубль
+//   'https://www.nbrb.by/api/exrates/rates/310',      // Тайский бат
+//   'https://www.nbrb.by/api/exrates/rates/302',      // Турецкая лира
+//   'https://www.nbrb.by/api/exrates/rates/130',      // Швейцарский франк
+//   'https://www.nbrb.by/api/exrates/rates/143',      // Фунт стерлингов
+//   'https://blockchain.info/ticker',                 // Bitcoin
+// ];
+// !  Конец
 
-const saves = document.querySelector('.saves'),
-  amountInput = document.querySelector('.amount-input'),
-  sendBtn = document.querySelector('#send'),
-  header = document.querySelector('.header'),
-  pattern = /[^0-9]/g;
-// 
-amountInput.oninput = function () {
-  this.value = this.value.replace(pattern, '');
-}
+const savingsBlock = document.querySelector('.savings');  //  блок со сбережениями
+const currenciesBlock = document.querySelector('.currencies');  //  блок с курсами валют
+const showFormButton = document.querySelector('.show-form-button'); //  кнопка, которая показывает форму
+const popup = document.querySelector('.popup'); //  весь блок с формой
+const form = document.querySelector('.form'); //  форма
+const savingAmountInputElem = document.querySelector('.saving-amount-input'); //  поле ввода
+const radioInput = document.querySelectorAll('.radio-input');   //  все радио кнопки
 
-
-
-
-// ? ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ 
-const savesTemplate = save => {
-  return `
-    <div class="save">
-      <span class="save__currency">${save.name}</span><span class="save__amount">${save.amount}</span><span class="save__symbol">${save.symbol}</span>
-    </div>
-  `
-}
-fetch('https://e-g-or.github.io/currencies/asource/saves.js')
-  .then(response => response.json())
-  .then(data => {
-    // console.log(data);
-    data.forEach(element => {
-      saves.innerHTML += savesTemplate(element);
-    });
-  })
-// ? ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼
-
-
-
-// ! ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ ▲ 
-const createTemplate = data => {
-  if ((data.Cur_Name)) return `
-    <div class="currency">
-      <h1 class="currency__name">Валюта: ${data.Cur_Name}</h1>
-      <p class="currency__value">Курс: <span class="value">${data.Cur_OfficialRate} Br</span></p>
-    </div>
-  `
-  else return `
-    <div class="currency">
-      <h1 class="currency__name">Валюта: Bitcoin</h1>
-      <p class="currency__value">Курс: <span class="value">${(data.USD).symbol} ${(data.USD).last} </span></p>
-    </div>
-  `
-}
-const requestURLs = [
-  'https://www.nbrb.by/api/exrates/rates/292',      // Евро
-  'https://www.nbrb.by/api/exrates/rates/145',      // Доллар США
-  // 'https://www.nbrb.by/api/exrates/rates/298',      // Российский рубль
-  'https://www.nbrb.by/api/exrates/rates/310',      // Тайский бат
-  // 'https://www.nbrb.by/api/exrates/rates/302',      // Турецкая лира
-  // 'https://www.nbrb.by/api/exrates/rates/130',      // Швейцарский франк
-  // 'https://www.nbrb.by/api/exrates/rates/143',      // Фунт стерлингов
-  'https://blockchain.info/ticker',                 // Bitcoin
-];
-requestURLs.forEach(url => {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      header.innerHTML += createTemplate(data);
-    });
+showFormButton.addEventListener('click', () => {
+  popup.classList.add('active');
 });
-// ! ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ ▼ 
-
-
-
-sendBtn.addEventListener('click', (e) => {
-  console.log("click");
-  let amount = amountInput.value;
-  if (amount != '') {
-    console.log(amount);
-  } else console.log("type something, idiot");
-  // if (e.target.closest("#send")) console.log("кнопка"); else console.log("не кнопка");
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    popup.classList.remove('active');
+  }
+});
+popup.addEventListener('click', e => {
+  if (!(e.target.closest('.popup__window'))) {
+    popup.classList.remove('active');
+  }
 })
